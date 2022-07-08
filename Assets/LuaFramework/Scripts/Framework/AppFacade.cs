b2e72c052ad4ace1a8087303760c12546aea829e
lym <123123>
2022-07-08 09:55:02 +0800

    测试和注解

--- Assets/LuaFramework/Scripts/Framework/AppFacade.cs
+++ Assets/LuaFramework/Scripts/Framework/AppFacade.cs
@@ -3,9 +3,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class AppFacade : Facade
+public class AppFacade : Facade // 框架管理类
 {
-    private static AppFacade _instance;
+    private static AppFacade _instance; // 单例
 
     public AppFacade() : base()
     {
@@ -23,16 +23,16 @@ public class AppFacade : Facade
 
     override protected void InitFramework()
     {
-        base.InitFramework();
-        RegisterCommand(NotiConst.START_UP, typeof(StartUpCommand));
+        base.InitFramework(); // 基类中初始化框架
+        RegisterCommand(NotiConst.START_UP, typeof(StartUpCommand)); // 把命令 注册或者存入 命令中心  其实是一个字典；之后输入 启动命令就可以 执行实例化对应的 类实例；
     }
 
     /// <summary>
     /// 启动框架
     /// </summary>
     public void StartUp() {
-        SendMessageCommand(NotiConst.START_UP);
-        RemoveMultiCommand(NotiConst.START_UP);
+        SendMessageCommand(NotiConst.START_UP); // 发送命令启动简讯，  他会从命令中心去获取到之前 注册的 类型
+        RemoveMultiCommand(NotiConst.START_UP); // 至于为什么要移除？ 是什么意思？
     }
 }
 
